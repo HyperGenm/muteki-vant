389be540a147468a896be2f8e3f3db5208f3170c
WeiziPlus <1602802727@qq.com>
2021-01-28 09:30:50 +0800

    1.0.5  底部tabbar新增自定义图片

--- src/components/tabbar/Index.vue
+++ src/components/tabbar/Index.vue
@@ -1,10 +1,21 @@
 <template>
     <van-tabbar v-model="activeTab">
-        <template v-for="(item,index) in tabbar.list" :key="item.name">
-            <van-tabbar-item :name="item.name"
-                             :to="`/${item.name}`"
-                             :icon="item.icon">{{item.title}}
-            </van-tabbar-item>
+        <template v-for="item in tabbar.list" :key="item.name">
+            <template v-if="!item.slot">
+                <van-tabbar-item :name="item.name"
+                                 :to="`/${item.name}`"
+                                 :icon="item.icon">{{item.title}}
+                </van-tabbar-item>
+            </template>
+            <template v-else>
+                <van-tabbar-item :name="item.name"
+                                 :to="`/${item.name}`">
+                    <span>{{item.title}}</span>
+                    <template #icon="props">
+                        <img :src="activeTab === item.name ? item.activeImg : item.unActiveImg"/>
+                    </template>
+                </van-tabbar-item>
+            </template>
         </template>
     </van-tabbar>
 </template>
@@ -21,13 +32,21 @@
         },
         props: {
             activeTab: {
-                type: String
+                type: String,
+                default: 'home'
             }
         },
         setup() {
             let tabbar = reactive({
                 list: [
                     {title: '首页', icon: 'home-o', name: 'home'},
+                    {
+                        title: '自定义', slot: true, name: 'customize',
+                        //网络图片
+                        activeImg: 'https://img.yzcdn.cn/vant/user-active.png',
+                        //本项目图片
+                        unActiveImg: require('@/assets/logo.png'),
+                    },
                     {title: '我的', icon: 'search', name: 'me'},
                 ]
             });
@@ -37,3 +56,15 @@
         }
     }
 </script>
+
+<style lang="less" scoped>
+    .van-tabbar {
+        .van-tabbar-item {
+            .van-tabbar-item__icon {
+                img {
+                    width: 30px;
+                }
+            }
+        }
+    }
+</style>
